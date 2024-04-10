9d300220f50014de499cfc122866b702bb2fa294
Andriy Voznyuk <andreyv@ukr.net>
2024-04-10 02:36:13 +0200

    Update dev.py

--- backend/core/settings/dev.py
+++ backend/core/settings/dev.py
@@ -6,8 +6,17 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-+$d#&btb)wb$71i1#0eellbko%#m1+bfm^ek+%(h^9hqo8od9d"
 
-# SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "selected12309.svitlo.tv",
+    "svitlo.tv",
+    "10.1.100.173",
+    "127.0.0.1",
+    "localhost",
+]
+CSRF_TRUSTED_ORIGINS = [
+    'https://selected12309.svitlo.tv/',
+    'http://10.1.100.173:8111/',
+]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
