434514fec9f5b48d552a1f9439729c825c2fb22a
Andriy Voznyuk <andreyv@ukr.net>
2024-04-13 00:54:59 +0200

    split chart on days

--- backend/tvweek/models.py
+++ backend/tvweek/models.py
@@ -5,8 +5,6 @@ import datetime as dt
 import pandas as pd
 import re
 
-from django.utils import timezone
-from django.db.models import F
 from django.db.models.functions import ExtractWeek, ExtractYear
 from django.utils.translation import gettext_lazy as _
 from django.db import models
@@ -21,6 +19,9 @@ class ChartLine(models.Model):
     program_genre = models.CharField(_("Жанр програми"), max_length=100, blank=True, null=True)
     project_of_program = models.ForeignKey('project.Project', blank=True, on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        return str(self.start_time)
+
 
 class TVChart(models.Model):
     tv_chart_file = models.FileField(max_length=255)
@@ -32,18 +33,29 @@ class WeekChart(Page):
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
-        now = datetime.now()
-        day_of_week = now.weekday()
-        week_number = now.isocalendar().week
-        year = now.year
+        now_day = datetime.now()
+        day_of_week = now_day.weekday()
+        week_number = now_day.isocalendar().week
+        year = now_day.year
         # get week titles
-        start_week, end_week, week_day = get_current_week_range(now)
-
+        start_week, end_week, week_day = get_current_week_range(now_day)
 
         chart_lines = ChartLine.objects.filter(start_time__date__range=(start_week, end_week))
 
-        context['week_days'] = generate_week_dict(now)
-        context['chart_lines'] = chart_lines
+        this_week_days = [{
+            'day_name': (start_week + dt.timedelta(days=week_day_number)).strftime('%A'),
+            'day_date': (start_week + dt.timedelta(days=week_day_number)),
+            'chart_lines': []
+        } for week_day_number in range(7)]
+
+        for line_temp in chart_lines:
+            weekday_index = line_temp.start_time.weekday()
+            this_week_days[weekday_index]['chart_lines'].append(line_temp)
+
+        context['week_days'] = generate_week_dict(now_day)
+        context['days_of'] = this_week_days
+        for oker in context['days_of']:
+            print(f'{oker=}')
 
         return context
 
