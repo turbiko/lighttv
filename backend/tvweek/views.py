83cd36c9591b533b7d8bd987bf193dd83a20d985
Andriy Voznyuk <andreyv@ukr.net>
2024-04-13 00:54:40 +0200

    remove timezone

--- backend/tvweek/views.py
+++ backend/tvweek/views.py
@@ -1,26 +1,22 @@
 from datetime import datetime
 import datetime as dt
 import pandas as pd
-import re
 
 from django.db import transaction
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.utils import timezone
 
-from .forms import TVChartUploadForm
-
-from .models import ChartLine
 from project.models import Project
+from .forms import TVChartUploadForm
+from .models import ChartLine
 from .tools import get_date_from_cell_0, generate_week_dict
 
 
 def upload_chart(request):
-    print(f"upload_chart started  {timezone.now()}")
 
     TAB_NAME_IN_XLS = 'Укр'  # TODO: it was hardcoded, refactor it
     tv_chart_file = ''
-    selected_date = ""  # reset once before load file, continuous value for chart data control
+    selected_date = ""  # reset once before load file, continuous value for chart line date control
 
     if request.method == 'POST':
         form = TVChartUploadForm(request.POST, request.FILES)
@@ -34,7 +30,6 @@ def upload_chart(request):
                     past_time = False  # for new day reset next day checker
                     string_dict_date = get_date_from_cell_0(row.iloc[0])
                     selected_date = datetime.strptime(string_dict_date['date_str'].strip(), '%d.%m.%Y').date()
-                    print(f'{selected_date=}')
                     continue
                 if isinstance(row.iloc[0], dt.time):
                     if not past_time:
@@ -44,7 +39,6 @@ def upload_chart(request):
                     elif past_time > row.iloc[0]:
                         past_time = row.iloc[0]
                         selected_date += dt.timedelta(days=1)
-                        print(f' {past_time=} {row.iloc[0]=} {selected_date=}')
 
                     if pd.isnull(row.iloc[1]) and pd.isnull(row.iloc[2]):  # when time exist but other data not exist
                         continue
@@ -55,7 +49,6 @@ def upload_chart(request):
                     chart_line_project_of_program = None  # reset value before checking
                     if projects_count > 1:
                         for project in related_projects:
-                            print(f"{project.chart_name_short=} | {chart_line.program_title=}")
                             if project.chart_name_short in chart_line.program_title:
                                 chart_line_project_of_program = project
                                 break
@@ -65,9 +58,7 @@ def upload_chart(request):
                     # finalize process by saving chart_line model element
                     chart_line_weekday = selected_date.weekday()
 
-                    chart_line_date = timezone.make_aware(datetime.combine(selected_date,
-                                                                           row.iloc[0]),
-                                                          timezone.get_default_timezone())
+                    chart_line_date = datetime.combine(selected_date,row.iloc[0])
                     chart_line, _temp = ChartLine.objects.update_or_create(
                         start_time=chart_line_date,
                         day_of_week=chart_line_weekday,
@@ -78,22 +69,18 @@ def upload_chart(request):
                     # chart_line.save() # ChartLine.objects.update_or_create make it automatically
     else:
         form = TVChartUploadForm()
+    messages.success(request, 'TV Chart data uploaded successfully.')
 
     # clear chart_line table from old elements
-    threshold_date = timezone.now() - dt.timedelta(days=60)  #TODO: hardcoded move to site settings
+    threshold_date = dt.datetime.now() - dt.timedelta(days=60)  #TODO: hardcoded move to site settings
     ChartLine.objects.filter(start_time__lt=threshold_date).delete()
-    print(f"chart_page complete {timezone.now()}")
     return render(request, 'tvweek/upload_chart.html', {'form': form})
 
 
 def chart_page(request):
-    print(f"chart_page started {timezone.now()}")
     context = {}
     now_day_is = datetime.datetime.now()
-    print(f"{now_day_is=}")
     # week days dict {week_day_name: Пн Вт Ср Чт Пт Сб Нд, detestamp}
     week_days = generate_week_dict(now_day_is)
     context['week_days'] = week_days
-
-    print(f"chart_page finished {timezone.now()}")
     return render(request, 'tvweek/week_chart.html', context)
